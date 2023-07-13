820e0ebe58070e4c8a84fccc43939bcefe5ac94e
Shawdes <mark-mark2017@outlook.com>
2023-07-13 09:41:16 -0500

    Login

--- SOA-MVC/Controllers/HomeController.cs
+++ SOA-MVC/Controllers/HomeController.cs
@@ -20,16 +20,6 @@ namespace SOA_MVC.Controllers
 
         public IActionResult Index()
         {
-            ClaimsPrincipal claimuser = HttpContext.User;
-            string nombreUsuario = "";
-
-            if (claimuser.Identity.IsAuthenticated)
-            {
-                nombreUsuario = claimuser.Claims.Where(c => c.Type == ClaimTypes.Name)
-                    .Select(c => c.Value).SingleOrDefault();
-            }
-
-            ViewData["nombreUsuario"] = nombreUsuario;
 
             return View();
         }
